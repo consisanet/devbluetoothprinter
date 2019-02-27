2ce9c218f3dc825de711d845e42239326a688eb2
Diego Occhi <diego.occhi@devionn.com>
2019-02-27 10:16:27 -0300

    first commit

---
+++ www/BluetoothPrinter.js
@@ -0,0 +1,28 @@
+var exec = require('cordova/exec');
+
+module.exports = {
+   list: function(fnSuccess, fnError){
+      exec(fnSuccess, fnError, "DevBluetoothPrinter", "list", []);
+   },
+   connect: function(name, fnSuccess, fnError){
+      exec(fnSuccess, fnError, "DevBluetoothPrinter", "connect", [name]);
+   },
+   disconnect: function(fnSuccess, fnError){
+      exec(fnSuccess, fnError, "DevBluetoothPrinter", "disconnect", []);
+   },
+   print: function(str, fnSuccess, fnError){
+      exec(fnSuccess, fnError, "DevBluetoothPrinter", "print", [str]);
+   },
+   printText: function(str, fnSuccess, fnError){
+      exec(fnSuccess, fnError, "DevBluetoothPrinter", "printText", [str]);
+   },
+   printImage: function(str, fnSuccess, fnError){
+      exec(fnSuccess, fnError, "DevBluetoothPrinter", "printImage", [str]);
+   },
+   printPOSCommand: function(str, fnSuccess, fnError){
+      exec(fnSuccess, fnError, "DevBluetoothPrinter", "printPOSCommand", [str]);
+   },
+   idConnected: function(fnSuccess, fnError){
+      exec(fnSuccess, fnError, "DevBluetoothPrinter", "isConnected", []);
+   },
+};
